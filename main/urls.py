c7ea75aa773c76d244b3ac106bbab9a2ed404c0a
Lev <lev@ishoyev.com>
2026-09-13 23:07:57 +0200

    created all from HW 10

--- main/urls.py
+++ main/urls.py
@@ -1,20 +1,22 @@
 from django.urls import path
 
+
 from . import views
+from .views import api_products
 
 app_name = 'main'
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('about/', views.about, name='about'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('about/', views.AboutView.as_view(), name='about'),
     path('review/', views.review, name='review'),
-    path('product/<int:product_id>/<str:product_name>/', views.product_detail, name='product_detail'),
+    path('product/<int:product_id>/<str:product_name>/', views.ProductDetailView.as_view(), name='product_detail'),
     path('category/<int:category_id>/', views.category_detail, name='category_detail'),
-    path('free/', views.free, name='free'),
+
+    # path('free/', views.free, name='free'),
     path('new/', views.new, name='new'),
-
-
-
-
-
+    path('api/product/<int:product_id>/', views.api_product_detail, name='api_product_detail'),
+    path('free_products/', views.IndexView.as_view(), {'is_free': True}, name='free_products'),
+    path('paid_products/', views.IndexView.as_view(), {'is_free': False}, name='paid_products'),
+    path('api/products/', views.api_products, name='api_products')
 ]
--- main/views.py
+++ main/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.db.models import Q
 from django.core.paginator import Paginator
+from django.views import View
+from django.views.decorators.http import require_GET
+from django.http import JsonResponse
+from django.views.generic import TemplateView, ListView, DetailView
 
 from .models import Product
 from .models import Review
@@ -12,54 +16,153 @@ SORTS = {
         'name': 'name',
         'new': '-created_at',
     }
-def index(request):
-    q = request.GET.get('q', '')
-    sort = request.GET.get('sort')
+# @require_GET
+# def index(request, is_free=None):
+#     q = request.GET.get('q', '')
+#     sort = request.GET.get('sort')
+#
+#     if is_free is True:
+#         products = Product.objects.filter(price=0)
+#         title = 'Бесплатные товары:'
+#
+#     elif is_free is False:
+#         products = Product.objects.filter(price__gt=0)
+#         title = 'Платные товары:'
+#
+#     else:
+#         products = Product.objects.all()
+#         title = 'Наши продукты:'
+#
+#     if q:
+#         products = products.filter(
+#             Q(name__icontains=q) |
+#             Q(description__icontains=q)
+#         )
+#
+#         if sort is None:
+#             sort = 'name'
+#     else:
+#         if sort is None:
+#             sort = 'new'
+#
+#     products = products.order_by(SORTS.get(sort, '-created_at'))
+#
+#     daily_product = Product.objects.order_by('-price').first()
+#     categories = Category.objects.all()
+#
+#     paginator = Paginator(products, 5)
+#     page_number = request.GET.get('page')
+#     page_obj = paginator.get_page(page_number)
+#
+#     return render(request, 'main/index.html', {
+#         'q': q,
+#         'sort': sort,
+#         'title': title,
+#         'is_free': is_free,
+#         'page_obj': page_obj,
+#         'products': products,
+#         'daily_product': daily_product,
+#         'categories': categories,
+#     })
 
-    if q:
-        products = Product.objects.filter(
-            Q(name__icontains=q) |
-            Q(description__icontains=q)
-        )
+class IndexView(ListView):
+    model = Product
+    template_name = 'main/index.html'
+    context_object_name = 'products'
+    paginate_by = 5
 
-        if sort is None:
+    def get_queryset(self):
+        q = self.request.GET.get('q', '')
+        sort = self.request.GET.get('sort')
+        is_free = self.kwargs.get('is_free')
+
+        if is_free is True:
+            products = Product.objects.filter(price=0)
+        elif is_free is False:
+            products = Product.objects.filter(price__gt=0)
+        else:
+            products = Product.objects.all()
+
+        if q:
+            products = products.filter(
+                Q(name__icontains=q) |
+                Q(description__icontains=q)
+            )
+            if sort is None:
+                sort = 'name'
+        else:
+            if sort is None:
+                sort = 'new'
+
+        return products.order_by(SORTS.get(sort, '-created_at'))
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        q = self.request.GET.get('q', '')
+        sort = self.request.GET.get('sort')
+        is_free = self.kwargs.get('is_free')
+
+        if q and sort is None:
             sort = 'name'
-
-    else:
-        products = Product.objects.all()
-
-        if sort is None:
+        elif not q and sort is None:
             sort = 'new'
+        if is_free is True:
+            title = 'Бесплатные товары'
+        elif is_free is False:
+            title = 'Платные товары'
+        else:
+            title = 'Наши продукты'
+        context['q'] = q
+        context['sort'] = sort
+        context['title'] = title
+        context['is_free'] = is_free
+        context['daily_product'] = Product.objects.order_by('-price').first()
+        context['categories'] = Category.objects.all()
 
-    products = products.order_by(SORTS.get(sort, '-created_at'))
-    daily_product = Product.objects.order_by('-price').first()
-    categories = Category.objects.all()
+        return context
 
-    paginator = Paginator(products, 5)
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
+# @require_GET
+# def about(request):
+#     return render(request, 'main/about.html')
 
-    return render(request, 'main/index.html', {
-        'q': q,
-        'sort': sort,
-        'page_obj': page_obj,
-        'products': products,
-        'daily_product': daily_product,
-        'categories': categories,
-    })
-
-def about(request):
-    return render(request, 'main/about.html')
-
-def product_detail(request, product_id, product_name):
-    product = get_object_or_404(Product, id=product_id)
-
-    if product_name != product.name:
-        return redirect('main:product_detail', product.id, product.name)
-
-    return render(request, 'main/product_detail.html', {'product': product})
+class AboutView(TemplateView):
+    template_name = 'main/about.html'
 
 
+# @require_GET
+# def product_detail(request, product_id, product_name):
+#     product = get_object_or_404(Product, id=product_id)
+#
+#     if product_name != product.name:
+#         return redirect('main:product_detail', product.id, product.name)
+#
+#     return render(request, 'main/product_detail.html', {'product': product})
+
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'main/product_detail.html'
+    context_object_name = 'product'
+    pk_url_kwarg = 'product_id'
+
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object()
+
+        product_name = self.kwargs.get('product_name')
+
+        if product_name != self.object.name:
+            return redirect(
+                'main:product_detail',
+                self.object.id,
+                self.object.name
+            )
+
+        context = self.get_context_data(object=self.object)
+        return self.render_to_response(context)
+
+
+@require_GET
 def category_detail(request, category_id):
     category = get_object_or_404(Category, id=category_id)
     q = request.GET.get('q', '')
@@ -89,14 +192,17 @@ def category_detail(request, category_id):
     })
 
 
+@require_GET
 def review(request):
     reviews = Review.objects.all()
     return render(request, 'main/review.html', {'reviews': reviews})
 
-def free(request):
-    products = Product.objects.filter(price=0)
-    return render(request, 'main/free.html', {'products': products})
+# @require_GET
+# def free(request):
+#     products = Product.objects.filter(price=0)
+#     return render(request, 'main/free.html', {'products': products})
 
+@require_GET
 def new(request):
     products = Product.objects.order_by('-created_at')
 
@@ -108,3 +214,27 @@ def new(request):
         'products': products,
         'page_obj': page_obj
     })
+
+def api_product_detail(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    data = {
+        'id': product.id,
+        'name': product.name,
+        'description': product.description,
+        'price': product.price
+    }
+    return JsonResponse(data)
+
+
+def api_products(request):
+    products = Product.objects.all()
+    data = []
+    for product in products:
+        data.append({
+            'id': product.id,
+            'name': product.name,
+            'description': product.description,
+            'price': product.price,
+            'rating': product.rating
+        })
+    return JsonResponse(data, safe=False)
